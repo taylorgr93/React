827e91532a6022d88c10af79fa195409771d34c2
ctgonzalez <taylor.gonzalez@knesysplus.com>
2023-03-22 14:00:50 -0600

    useEffect curso react

---
+++ 05-hook-app/src/02-useEffect/SimpleForm.jsx
@@ -0,0 +1,55 @@
+import React, { useEffect, useState } from "react";
+
+export const SimpleForm = () => {
+  const [formState, setFormState] = useState({
+    username: "strider",
+    email: "fernando@google.com",
+  });
+
+  const { username, email } = formState;
+
+  const onInputChange = ({ target }) => {
+    const { name, value } = target;
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
+  };
+
+  useEffect(() => {
+    console.log("useEffect called");
+  }, []);
+
+  useEffect(() => {
+    console.log("formState change");
+  }, [formState]);
+
+  useEffect(() => {
+    console.log("email change");
+  }, [email]);
+
+  return (
+    <>
+      <h1>Formulario Simple</h1>
+      <hr />
+
+      <input
+        type="text"
+        className="form-control"
+        placeholder="Username"
+        name="username"
+        value={username}
+        onChange={onInputChange}
+      />
+
+      <input
+        type="email"
+        className="form-control mt-2"
+        placeholder="correo@google.com"
+        name="email"
+        value={email}
+        onChange={onInputChange}
+      />
+    </>
+  );
+};
--- 05-hook-app/src/main.jsx
+++ 05-hook-app/src/main.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { CounterApp } from "./01-useState/CounterApp";
+import { CounterWithCustomHook } from "./01-useState/CounterWithCustomHook";
 import { HooksApp } from "./HooksApp";
+import { SimpleForm } from "./02-useEffect/SimpleForm";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <HooksApp />
-  </React.StrictMode>
+  // <React.StrictMode>
+  <SimpleForm />
+  // </React.StrictMode>
 );
